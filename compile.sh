6b22404a11963c33077a6bc3fa1cfcae123148ed
alexthegreat194 <alexander.gentry@aol.com>
2021-12-24 20:51:31 -0800

    changed file structure

--- compile.sh
+++ compile.sh
@@ -9,4 +9,4 @@ g++ -c NotificationHandler.cpp
 g++ -c WeaponBox.cpp 
 g++ -c Emberblade.cpp
 g++ -c main.cpp 
-g++ main.o -o Emberblade Animal.o BodyBox.o Globals.o Inventory.o Item.o ItemStack.o Notification.o NotificationHandler.o WeaponBox.o Emberblade.o -lsfml-graphics -lsfml-window -lsfml-system
+g++ main.o -o ../Emberblade Animal.o BodyBox.o Globals.o Inventory.o Item.o ItemStack.o Notification.o NotificationHandler.o WeaponBox.o Emberblade.o -lsfml-graphics -lsfml-window -lsfml-system
